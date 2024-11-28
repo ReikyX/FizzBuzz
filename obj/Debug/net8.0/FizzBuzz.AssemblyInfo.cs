7f4f1f7667b2e69882f93f3a7c5a9d710bd32d1e
Fries <erik.fries@training.lug-ag.de>
2024-11-28 13:12:39 +0100

    bin Ordner entfernt

--- obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
+++ obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f5ce428250de789da22d297d738df306c805724")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a685bffce9cd6690c8ce5792bb4e4eace6cf33be")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
